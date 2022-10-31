2c16282175f3eab9f678919fbcdd768eee7bd9b1
TGuesdon <thomas.guesdon12@gmail.com>
2022-10-31 17:32:34 +0100

    config ts & npm

---
+++ build/generator.js
@@ -0,0 +1,5 @@
+"use strict";
+function generate(width, height) {
+    console.log(`Generating a ${width} x ${height} map`);
+}
+generate(10, 10);
---
+++ src/generator.ts
@@ -0,0 +1,6 @@
+
+function generate(width: number, height: number) {
+    console.log(`Generating a ${width} x ${height} map`);
+}
+
+generate(10,10);
